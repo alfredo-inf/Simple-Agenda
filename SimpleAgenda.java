5499fd5045420cb80fcfbb4132d1b02a37445125
Your Name <alfredoquezada0@gmail.com>
2024-01-24 20:48:40 -0400

    first commit

---
+++ SimpleAgenda.java
@@ -0,0 +1,19 @@
+public class SimpleAgenda {
+    public static void agregarContacto (String nombre) {
+
+    }
+    public static void removerContacto (int id) {
+
+    }
+
+    public static String actualizarContacto (int na){
+        return "N/A";
+    }
+
+    public static void mostrarContacto(){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
